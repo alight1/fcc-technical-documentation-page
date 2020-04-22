138e1dcf09f8690368deea3596cafb510a5e6040
alight1 <andrelight.al@gmail.com>
2020-04-21 18:38:40 -0600

    Set correct freeCodeCamp test suite

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import bodyContent from "../src/body-content.html";
 document.getElementById("body").innerHTML += bodyContent;
 
 //Append freeCodeCamp test utility (does not work when added directly to HTML)
-localStorage.setItem("project_selector", "product-landing-page");
+localStorage.setItem("project_selector", "technical-docs-page");
 const fccScript = document.createElement("script");
 fccScript.src =
   "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
